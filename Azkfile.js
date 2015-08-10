58e127352a028c32929238899034e7df9757de66
Julio Makdisse Saito <saitodisse@gmail.com>
2015-08-10 18:10:17 -0300

    [azk] deploy system and Azkfile.md readme

--- Azkfile.js
+++ Azkfile.js
@@ -1,58 +1,34 @@
-/* global systems sync persistent path */
+/* globals systems path sync persistent */
+/* eslint camelcase: [2, {properties: "never"}] */
+/* eslint comma-dangle: [0, {properties: "never"}] */
 
-// Documentation: http://docs.azk.io/Azkfile.js
-
-// Adds the systems that shape your system
+/* see Azkfile.md */
 systems({
+  /////////////////////////////////
+  /// isomorphic500 app
+  /// -----------------------------
+  /////////////////////////////////
   "isomorphic500": {
-    // Dependent systems
     depends: [],
-
-    // More images:  http://images.azk.io
     image: {docker: "azukiapp/node:0.12"},
-
-    // Steps to execute before running instances
-    // use reprovision to run again: `azk restart -R`
     provision: [
       "npm install"
     ],
     workdir: "/azk/#{manifest.dir}",
     shell: "/bin/bash",
-    command: "npm run dev",
-
-    // wait for port inside container
-    wait: {retry: 20, timeout: 1000},
+    command: "npm run build && npm run prod",
+    wait: {retry: 60, timeout: 1000},
     mounts: {
       "/azk/#{manifest.dir}": sync("."),
       "/azk/#{manifest.dir}/node_modules": persistent("#{manifest.dir}/node_modules")
     },
     scalable: {default: 1},
     http: {
-      domains: ["#{system.name}.#{azk.default_domain}"]
+      domains: [
+      "#{system.name}.#{azk.default_domain}", // default azk
+      "#{process.env.AZK_HOST_IP}"            // used if deployed
+    ]
     },
-    ports: {
-      http: "3000/tcp",
-      hot: "3001:3001/tcp"
-    },
-    envs: {
-      HOST_NAME: "#{system.name}.#{azk.default_domain}",
-      NODE_ENV: "dev",
-      PORT: "3000",
-      HOST: "0.0.0.0"
-    },
-    export_envs: {
-      APP_URL: "#{azk.default_domain}:#{net.port.http}"
-    }
-  },
-
-  "isomorphic500-prod": {
-    extends: "isomorphic500",
-    provision: [
-      "npm install"
-    ],
-    scalable: { default: 0, limit: 1 },
-    command: "npm run build && npm run prod",
-    wait: {retry: 40, timeout: 1000},
     ports: {
       http: "8080/tcp"
     },
@@ -61,62 +37,41 @@ systems({
       NODE_ENV: "production",
       PORT: "8080",
       HOST: "0.0.0.0"
-    },
-    export_envs: {
-      APP_URL: "#{azk.default_domain}:#{net.port.http}"
     }
   },
 
-  "ngrok": {
-    // Dependent systems
-    depends: ["isomorphic500"],
-    image: {docker: "azukiapp/ngrok"},
-
-    // Mounts folders to assigned paths
+  /////////////////////////////////
+  /// deploy
+  /// -----------------------------
+  /// https://github.com/azukiapp/docker-deploy-digitalocean
+  /////////////////////////////////
+  deploy: {
+    image: {"docker": "azukiapp/deploy-digitalocean"},
     mounts: {
-      // equivalent persistent_folders
-      "/ngrok/log": path("./log")
+      "/azk/deploy/src":  path("."),
+      "/azk/deploy/.ssh": path("#{process.env.HOME}/.ssh")
     },
-    scalable: { default: 0, limit: 1 },
-
-    // do not expect application response
-    wait: false,
-    http: {
-      domains: ["#{system.name}.#{azk.default_domain}"]
-    },
-    ports: {
-      http: "4040"
-    },
+    scalable: {"default": 0, "limit": 0},
     envs: {
-      NGROK_CONFIG: "/ngrok/ngrok.yml",
-      NGROK_LOG: "/ngrok/log/ngrok.log"
+      GIT_CHECKOUT_COMMIT_BRANCH_TAG: 'azkfile-deploy',
+      AZK_RESTART_COMMAND: 'azk restart -Rvv',
     }
   },
 
-  "ngrok-prod": {
-    // Dependent systems
-    depends: ["isomorphic500-prod"],
-    image: {docker: "azukiapp/ngrok"},
-
-    // Mounts folders to assigned paths
-    mounts: {
-      // equivalent persistent_folders
-      "/ngrok/log": path("./log")
-    },
-    scalable: { default: 0, limit: 1 },
-
-    // do not expect application response
-    wait: false,
-    http: {
-      domains: ["#{system.name}.#{azk.default_domain}"]
-    },
-    ports: {
-      http: "4040"
-    },
+  /////////////////////////////////
+  /// deploy fast
+  /// -----------------------------
+  /// skip setup and configure
+  /////////////////////////////////
+  "fast-deploy": {
+    extends: 'deploy',
     envs: {
-      NGROK_CONFIG: "/ngrok/ngrok.yml",
-      NGROK_LOG: "/ngrok/log/ngrok.log"
+      GIT_CHECKOUT_COMMIT_BRANCH_TAG: 'azkfile-deploy',
+      AZK_RESTART_COMMAND: 'azk restart -Rv',
+      RUN_SETUP: 'false',
+      RUN_CONFIGURE: 'false',
+      RUN_DEPLOY: 'true',
     }
-  }
+  },
 
 });
---
+++ Azkfile.md
@@ -0,0 +1,107 @@
+# isomorphic500 :: Azkfile.js
+
+### Systems
+
+#### 1) isomorphic500
+
+- main node.js web site (http://isomorphic500.dev.azk.io)
+
+--------------------
+
+## Start azk
+
+#### [Install azk](http://docs.azk.io/en/installation/README.html)
+
+```sh
+# easy install
+curl -Ls http://azk.io/install.sh | bash
+```
+
+#### start all systems
+
+```sh
+# easy start
+azk start
+```
+
+#### deploy to Digital Ocean
+
+```sh
+# commit your changes, and:
+
+azk shell deploy  # needed the first-time
+
+# OR
+azk shell fast-deploy
+```
+
+--------------------
+
+### Other azk commands
+
+#### stop all containers
+
+```sh
+$ azk stop
+```
+
+#### restart all container
+
+```sh
+$ azk restart
+```
+
+#### restart and reprovision all container
+
+```sh
+$ azk restart -Rvv
+```
+
+#### check logs
+
+```sh
+$ azk logs
+```
+
+#### info on containers
+
+```sh
+$ azk info
+```
+
+--------------------
+
+### more on azk
+
+- Official Site
+  http://azk.io
+- Github
+  https://github.com/azukiapp/azk
+- Documentation
+  http://docs.azk.io
+- Images directory created by the azk team
+  http://images.azk.io
+
+### Contribute to azk
+
+- Star azk on Github
+  https://github.com/azukiapp/azk
+- Report an issue
+  https://github.com/azukiapp/azk/issues/new
+- Help solving a reported issue
+  https://github.com/azukiapp/azk/issues
+- Check out our awesome sponsors
+  http://azk.io/#sponsors
+
+### Stay in touch with the azk team
+
+- Sign up the weekly digest
+  http://www.azk.io/#newsletter
+- Follow the blog
+  https://medium.com/azuki-news
+- Talk to our support (chat)
+  https://gitter.im/azukiapp/azk (English) e https://gitter.im/azukiapp/azk/pt (Português)
+- Facebook
+  https://www.facebook.com/azukiapp
+- Twitter
+  http://twitter.com/azukiapp
